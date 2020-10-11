d13e5f37f5ccc9587b36faedcbaccd849e44af04
porosh2002 <jamilkashem2002@gmail.com>
2020-10-11 22:27:25 +0600

    a

--- Server.js
+++ Server.js
@@ -69,7 +69,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(helmet());
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('/', function(req, res) {
+app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 // Database Model
